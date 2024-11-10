cdb99843bc301701c8938543c7c2cb71cae29864
Sungwoo <bsungwoo@snu.ac.kr>
2024-11-11 00:58:28 +0900

    Fix bug

--- src/topological_comp.cpp
+++ src/topological_comp.cpp
@@ -305,6 +305,7 @@ Eigen::VectorXd topological_comp_res(
     threshold.erase(std::unique(threshold.begin(), threshold.end()), threshold.end());
 
     auto CC_list = extract_connected_comp(t, A, threshold, p, min_size);
+    std::cout << "The value of CC_list is: " << CC_list << std::endl;
 
     Eigen::SparseMatrix<double> CC_loc_mat = extract_connected_loc_mat(CC_list, p, "sparse");
     CC_loc_mat = filter_connected_loc_exp(CC_loc_mat, feat, thres_per);
