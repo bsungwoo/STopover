68c8ddfa2a0797a62a74fca85bb99939e017773e
Sungwoo <bsungwoo@snu.ac.kr>
2024-11-16 00:08:37 +0900

    Fix error

--- src/make_original_dendrogram.h
+++ src/make_original_dendrogram.h
@@ -5,19 +5,22 @@
 #include <tuple>
 #include <set>
 #include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <stdexcept>
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 
 // Function to create the original dendrogram
 std::tuple<
     std::vector<std::vector<int>>,        // CC: Connected Components
-    Eigen::SparseMatrix<double>,          // E: Connectivity Matrix between CCs
+    Eigen::SparseMatrix<double, Eigen::RowMajor>,          // E: Connectivity Matrix between CCs
     Eigen::MatrixXd,                      // duration: Birth and Death of CCs
     std::vector<std::vector<int>>         // history: History of CCs
 >
 make_original_dendrogram_cc(
     const Eigen::VectorXd& U,
-    const Eigen::SparseMatrix<double>& A,
+    const Eigen::SparseMatrix<double, Eigen::RowMajor>& A,
     const std::vector<double>& threshold
 );
 
